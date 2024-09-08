5cab335717f78338bff73248080ce92718840bc5
Jiraya <177346249+intjiraya@users.noreply.github.com>
2024-09-08 20:00:15 +0300

    First commit

--- AdminAssist/Commands/Call.cs
+++ AdminAssist/Commands/Call.cs
@@ -1,29 +1,96 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CommandSystem;
 using Exiled.API.Features;
-using RemoteAdmin;
+using Exiled.Permissions.Extensions;
 
 namespace AdminAssist.Commands
 {
+    [CommandHandler(typeof(ClientCommandHandler))]
     public class Call : ICommand
     {
-        public string Command { get; } = Plugin.Instance.Config.CommandAliases[0];
-        // Get all aliases except the primary command
-        public string[] Aliases { get; } = Plugin.Instance.Config.CommandAliases.Length > 1 ? Plugin.Instance.Config.CommandAliases.Skip(1).ToArray() : new string[] { };
-        public string Description { get; } = "Call the admin.";
+        public string Command { get; } = Plugin.Instance.Config.CommandAliases.First();
+        public string[] Aliases { get; } = Plugin.Instance.Config.CommandAliases.Count > 1
+            ? Plugin.Instance.Config.CommandAliases.Skip(1).ToArray()
+            : new string[] { };
+        public string Description { get; } = "Call the admin for assistance.";
+
+        private static readonly Dictionary<string, DateTime> LastCommandUsage = new Dictionary<string, DateTime>();
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-            var player = Player.Get(sender);
-            if (player == null)
+            var callingPlayer = Player.Get(sender);
+            if (callingPlayer == null)
             {
                 response = "You need to be a player to call an admin.";
                 return false;
             }
 
-            response = "Admin not called";
-            return false;
+            string playerId = callingPlayer.UserId;
+            float cooldown = Plugin.Instance.Config.Cooldown;
+
+            if (cooldown > 0)
+            {
+                DateTime currentTime = DateTime.Now;
+
+                if (LastCommandUsage.TryGetValue(playerId, out DateTime lastUsageTime))
+                {
+                    TimeSpan elapsedTime = currentTime - lastUsageTime;
+
+                    if (elapsedTime.TotalSeconds < cooldown)
+                    {
+                        double timeLeft = cooldown - elapsedTime.TotalSeconds;
+                        response = $"You must wait {timeLeft:F1} seconds before using this command again.";
+                        return false;
+                    }
+                }
+
+                LastCommandUsage[playerId] = currentTime;
+            }
+
+            string argumentsStr = string.Join(" ", arguments).Replace("<", "[").Replace(">", "]");
+
+            HashSet<Player> receivers = Player.List
+                .Where(p => Plugin.Instance.Config.Permissions.Any(permission => p.CheckPermission(permission)))
+                .ToHashSet();
+
+            if (Plugin.Instance.Config.ToAllRaAuthorized)
+            {
+                receivers.UnionWith(Player.List.Where(p => p.RemoteAdminAccess));
+            }
+
+            if (!string.IsNullOrEmpty(Plugin.Instance.Config.AdminsBroadcast))
+            {
+                string broadcastMessage = Plugin.Instance.Config.AdminsBroadcast
+                    .Replace("%nickname%", callingPlayer.Nickname)
+                    .Replace("%steamid%", callingPlayer.UserId)
+                    .Replace("%id%", callingPlayer.Id.ToString())
+                    .Replace("%value%", argumentsStr);
+
+                foreach (var receiver in receivers)
+                {
+                    receiver.Broadcast(Plugin.Instance.Config.AdminsBroadcastDuration, broadcastMessage);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(Plugin.Instance.Config.ConsoleLog))
+            {
+                string consoleMessage = Plugin.Instance.Config.ConsoleLog
+                    .Replace("%nickname%", callingPlayer.Nickname)
+                    .Replace("%steamid%", callingPlayer.UserId)
+                    .Replace("%id%", callingPlayer.Id.ToString())
+                    .Replace("%value%", argumentsStr);
+
+                foreach (var receiver in receivers)
+                {
+                    receiver.SendConsoleMessage(consoleMessage, "Silver");
+                }
+            }
+
+            response = "Admins have been notified!";
+
+            return true;
         }
     }
 }
--- AdminAssist/Config.cs
+++ AdminAssist/Config.cs
@@ -10,43 +10,35 @@ namespace AdminAssist
         [Description("Enables or disables the plugin.")]
         public bool IsEnabled { get; set; } = true;
 
-        [Description("Enables or disables debug mode for the plugin.")]
+        [Description("Enables or disables debug mode for the plugin. This will log additional information for troubleshooting.")]
         public bool Debug { get; set; } = false;
 
-        [Description("List of command aliases that can trigger the admin assist functionality.\nAlways put the original name of command first.")]
-        public string[] CommandAliases { get; set; } = new string[] { "call", "admin", };
+        [Description("A set of command aliases that can trigger the admin assist functionality.\n" +
+                     "Always put the original name of the command first!\n")]
+        public HashSet<string> CommandAliases { get; set; } = new HashSet<string> { "call", "admin" };
 
         [Range(0, float.MaxValue)]
-        [Description("Cooldown time (in seconds) for the command to prevent spam.")]
+        [Description("Cooldown time (in seconds) for the command to prevent spam. Set to 0 to disable cooldown.")]
         public float Cooldown { get; set; } = 0f;
 
-        [Description("List of permissions that will receive requests from players.")]
-        public List<string> Permissions { get; set; } = new List<string> { "PlayersManagement", };
+        [Description("A set of permissions that will receive requests from players.\n" +
+                     "These permissions should correspond to RA (Remote Admin) permission levels.")]
+        public HashSet<string> Permissions { get; set; } = new HashSet<string> { "CallNotify", "PlayerSensitiveDataAccess" };
 
-        [Description("Should all players with RA (Remote Admin) permissions receive requests?")]
+        [Description("If true, all players with RA (Remote Admin) permissions will receive requests.")]
         public bool ToAllRaAuthorized { get; set; } = true;
 
-        [Description("Specifies whether a prefix is required for commands.")]
-        public bool IsPrefixRequired { get; set; } = false;
+        [Description("Template for logging information to the receiver's console.\n" +
+                     "Available keywords: %steamid% - Sender's Steam ID, %nickname% - Sender's Steam nickname, %value% - Arguments, %id% - Sender's player ID, %type% - Prefix type (empty if no prefix is used and not required).\n" +
+                     "Set to an empty string to disable.")]
+        public string ConsoleLog { get; set; } = "%nickname% => %value%";
 
-        [Description("Enables the prefix system for commands.")]
-        public bool IsPrefixesEnabled { get; set; } = true;
+        [Description("The message broadcasted to admins when a player calls for assistance.\n" +
+                     "Use %id% for the player's ID and %name% for the player's name.\n" +
+                     "Set to an empty string to disable")]
+        public string AdminsBroadcast { get; set; } = "<color=#FFA500>(%id%) %nickname% called the admins</color>";
 
-        [Description("Dictionary of available prefixes for reporting commands.")]
-        public IReadOnlyDictionary<string, string> AvailablePrefixes { get; set; } = new Dictionary<string, string>
-        {
-            { "!", "report" },
-            { "?", "question" },
-            { "*", "request" },
-        };
-
-        [Description("Plugin language. Available languages: en, ru, pl.")]
-        public string Language { get; set; } = "en";
-
-        [Description("Available keywords: %name% - Sender's Steam nickname, %value% - Arguments, %id% - Sender's player ID, %type% - Type based on the prefix (empty if no prefix and prefixes are not required).")]
-        public string ConsoleLog { get; set; } = "%name% => %value%";
-
-        [Description("Broadcast message sent to admins when a player calls for assistance.")]
-        public string AdminsBroadcast { get; set; } = "<color=yellow><b>(%id%) %name% called the admins</b></color>";
+        [Description("The duration (in seconds) of the broadcast message. Set to 0 to disable")]
+        public ushort AdminsBroadcastDuration { get; set; } = 5;
     }
 }
